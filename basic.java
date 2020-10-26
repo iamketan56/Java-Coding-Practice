0d70d21b4ceb42c95e46965f35c9e578b0b13462
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-26 08:12:02 +0530

    Java Basic Getter and Setter

---
+++ basic.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class basic {
+    private String name;
+    private int age;
+    private String color;
+ public void simple()
+ {
+     System.out.println("hi");
+ }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
