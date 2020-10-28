b392b66e8e84f9fd9ca9be851cc590d2eb1127c6
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-28 08:30:42 +0530

    Basic

---
+++ Basic 2.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+      int a=20;
+      byte b= (byte) a;
+        int c=2345654;
+        int d=c;
+        System.out.println(a+" " +b+" "+c+" "+d);
+    }
+}
