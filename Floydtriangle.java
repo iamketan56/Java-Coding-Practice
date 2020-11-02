f4a763962639267997611ca01041ba7a7ee156a0
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-02 07:17:23 +0530

    Create Floydtriangle.java

---
+++ Floydtriangle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Floydtriangle {
+    public static void main(String args[])
+    {   int k=1;
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
