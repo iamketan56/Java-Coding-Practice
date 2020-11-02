6adb7656775b2ffa38a1cbc34db00c31db6c230f
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-02 07:24:39 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Pattern {
+    public static void main(String agrs[])
+    {
+        byte k=1;
+        for(byte i=1;i<=5;i++)
+        {
+            for(byte j=1;j<=5;j++)
+            {
+
+                System.out.print(" "+k);
+
+            }
+            System.out.println();
+            k++;
+        }
+    }
+}
